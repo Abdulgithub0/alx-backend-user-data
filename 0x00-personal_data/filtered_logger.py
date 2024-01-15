00bc6c9df93c3356595027cbc0e04b91363ef2e2
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-01-15 22:56:21 +0100

    obsfucated a data using regex

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,21 +8,16 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List,
-                 redaction: str,
-                 message: str,
-                 separator: str
-                 ) -> str:
+def filter_datum(fields: List, redaction: str,
+                 message: str, separator: str) -> str:
     """
-    :params:
-        fields: a list of strings representing all fields
-            to obfuscate
-        redaction: a string representing by what
-            the field will be obfuscated
-        message: a string representing the log line
-        separator: a string representing by which character
-            is separating all fields in the log line (message)
-    :return: strings of encrypt data based on redaction supply
+    fields: a list of strings representing all fields
+        to obfuscate
+    redaction: a string representing by what
+        the field will be obfuscated
+    message: a string representing the log line
+    separator: a string representing by which character
+    is separating all fields in the log line (message)
     """
     for data in fields:
         message = re.sub(r"{}=[^{}]*".format(data, separator),
