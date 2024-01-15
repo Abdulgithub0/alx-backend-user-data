1672aca3552c4fc11261bb1456a42ae5fb57a0b8
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-01-15 23:19:58 +0100

    subclass logging.Handler class

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -23,3 +23,22 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(r"{}=[^{}]*".format(data, separator),
                          "{}={}".format(data, redaction), message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """obsfucate the value of attr message on record
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            record.message, self.SEPARATOR)
