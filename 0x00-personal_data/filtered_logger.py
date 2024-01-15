8a6add61cf9e59054e4344d570fa65a0986cdc16
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-01-15 22:46:46 +0100

    obsfucated a data using regex

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Encrypt user data
+"""
+
+import bcrypt as b
+import logging
+import re
+from typing import List
+
+
+def filter_datum(fields: List,
+                 redaction: str,
+                 message: str,
+                 separator: str
+                 ) -> str:
+    """
+    :params:
+        fields: a list of strings representing all fields
+            to obfuscate
+        redaction: a string representing by what
+            the field will be obfuscated
+        message: a string representing the log line
+        separator: a string representing by which character
+            is separating all fields in the log line (message)
+    :return: strings of encrypt data based on redaction supply
+    """
+    for data in fields:
+        message = re.sub(fr"{data}=[^{separator}]*",
+                         f"{data}={redaction}", message)
+    return message
