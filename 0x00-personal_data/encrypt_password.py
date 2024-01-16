450cd936db4989f6353cba25fcebbd3729dd4e84
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-01-16 10:07:53 +0100

    func that return an encrypted string

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ def hash_password(password: str) -> bytes:
     """return a salted byte encrypted version of password
     """
     return b.hashpw(password.encode("utf-8"), b.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) - bool:
+    """validate that the provided password matches the hashed password
+    """
+    return b.checkpw(password.encode("utf-8"), hashed_password)
