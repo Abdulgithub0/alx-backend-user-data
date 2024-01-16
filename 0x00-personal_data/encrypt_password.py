221008330d554ef9c307484b0bff48a59c12cc3f
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-01-16 10:02:59 +0100

    func that return an encrypted string

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Encrypt user passwords
 """
-
 import bcrypt as b
 
 
-def hash_password(psword: str) -> bytes:
-    """return salted byte encrypted version of pword"""
-    return b.hashpw(psword.encode("utf-8"), b.gensalt())
+def hash_password(password: str) -> bytes:
+    """return a salted byte encrypted version of password
+    """
+    return b.hashpw(password.encode("utf-8"), b.gensalt())
