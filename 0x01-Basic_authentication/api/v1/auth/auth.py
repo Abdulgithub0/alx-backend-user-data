96457def75bdcea3f4e5b043b6ed406312d35043
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-01-17 13:45:29 +0100

    wrote the  template class for Basic access authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Implement Basic access Authorization for the server app
+"""
+from flask import request
+from typing import Typevar
+from models.user import User
+
+
+class Auth:
+    """Template for all authentication system used in flask app instance.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """will """
+        return False
+
+    def authorization_header(self, request: TypeVar('request') = None) -> str:
+        """ later """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """later also"""
+        return None
