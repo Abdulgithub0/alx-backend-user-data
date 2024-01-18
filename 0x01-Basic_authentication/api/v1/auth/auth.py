bf3466157feb0d30e4a4d7c63a3b3cb7b81c0b64
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-01-18 11:07:35 +0100

    implemented an api requests filtering

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Implement Basic access Authorization for the server app
+"""Implement Authorization for the api server
 """
 from flask import request
 from typing import TypeVar, List
@@ -7,7 +7,7 @@ from models.user import User
 
 
 class Auth:
-    """Template for all authentication system used in flask app instance.
+    """Template for all authentication system used in api server
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Define which routes don't need authentication"""
@@ -19,8 +19,9 @@ class Auth:
         return real
 
     def authorization_header(self, request: TypeVar('request') = None) -> str:
-        """ later """
-        return None
+        """validate all requests to secure the API"""
+        valid = request.headers.get("Authorization") if request else None
+        return valid
 
     def current_user(self, request=None) -> TypeVar('User'):
         """later also"""
