a92a2593f315715c365173fc8b35db4b266a31cf
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-01-18 12:41:17 +0100

    decode functionalities

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """implement Basic Access Authentication Protocol
 """
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -12,9 +13,21 @@ class BasicAuth(Auth):
         """ Extract a substring(base64 type) from a string.
         """
         _base64 = None
-        if authorization_header and type(authorization_header) is str:
+        if authorization_header and isinstance(authorization_header, str):
             if (authorization_header.startswith("Basic ")
                and not ("Basic  " in authorization_header)):
-                _base64 = authorization_header.split()
-                _base64 = _base64[1] if len(_base64) == 2 else None
+                _base64 = authorization_header[6:]
         return _base64
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """ decode a base64 str
+        """
+        decode = None
+        if (base64_authorization_header and isinstance(base64_authorization_header, str)):
+            try:
+                decode = b64decode(base64_authorization_header).decode("utf-8")
+            except Exception:
+                decode = None
+        return decode
