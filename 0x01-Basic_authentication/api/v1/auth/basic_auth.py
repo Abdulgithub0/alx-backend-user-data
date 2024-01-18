b0dc01883bd985d683f07d094ae33cc2200e3563
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-01-18 12:01:31 +0100

    implemented a method that extract base64 from str

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,16 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """Subclass Auth template and added BasicAuth protocol spec interfaces
+    """Subclass Auth template and define  BasicAuth protocol.
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Extract a substring(base64 type) from a string.
+        """
+        _base64 = None
+        if authorization_header and type(authorization_header) is str:
+            if (authorization_header.startswith("Basic ")
+               and not ("Basic  " in authorization_header)):
+                _base64 = authorization_header.split()
+                _base64 = _base64[1] if len(_base64) == 2 else None
+        return _base64
