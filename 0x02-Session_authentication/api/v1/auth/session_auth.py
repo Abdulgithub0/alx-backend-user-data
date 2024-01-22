74f8793cbf2e4aa07a063aa27a0d652386cef93b
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-01-22 14:45:28 +0100

    created a session class that handle user sessions

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -19,3 +19,10 @@ class SessionAuth(Auth):
             self.user_id_by_session_id[session_id] = user_id
             return session_id
         return None
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """retrive user_id from store session_id
+        """
+        if session_id and isinstance(session_id, str):
+            return self.user_id_by_session_id.get(session_id)
+        return None
