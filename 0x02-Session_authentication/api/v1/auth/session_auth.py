7da4fa89eac53fd13949ee63f34a9cce1784775f
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-01-22 14:39:18 +0100

    created a session class that handle user sessions

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,19 @@
 """
 
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """define session protocols needed for the app services
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """handle the creation of unique session per user id
+        """
+        if user_id and type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
+        return None
