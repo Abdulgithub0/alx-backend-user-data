e2307a11efcd8e3cc4d425d338d99efee05656bc
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-01-25 12:32:47 +0100

    implemented time expiration on sessions

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -11,6 +11,7 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -23,6 +24,8 @@ elif auth and auth == "session_auth":
     auth = SessionAuth()
 elif auth and auth == "session_exp_auth":
     auth = SessionExpAuth()
+elif auth auth == "session_db_auth":
+    auth = SessionDBAuth()
 else:
     auth = Auth()
 
