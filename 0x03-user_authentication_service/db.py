3e9fc3a3f0f37607d3a582693a7b043c2e4f914e
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-01-28 23:13:03 +0100

    define create user func

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,10 +30,10 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email_: str, hashed_password_: str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """add user to db
         """
-        user = User(email=email_, hashed_password=hashed_password_)
+        user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
