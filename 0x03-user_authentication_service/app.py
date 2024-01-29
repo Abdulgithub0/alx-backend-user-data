56dcc641287356f2a4a1025efc8b7b529aff0f9b
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-01-29 15:51:26 +0100

    fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -96,7 +96,7 @@ def reset_password_token():
         token = AUTH.get_reset_password_token(email)
     except ValueError:
         abort(403)
-    return jsonify({"email": email, "reset_token": token})
+    return jsonify({"email": email, "reset_token": token}), 200
 
 
 if __name__ == "__main__":
