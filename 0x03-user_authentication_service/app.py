0c709d9b9cd358f8907eba5e0c5777dfffa70433
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-01-29 17:07:15 +0100

    added feature that protected password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -100,5 +100,22 @@ def reset_password_token():
     return jsonify({"email": email, "reset_token": token})
 
 
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password():
+    """PUT /reset_password
+       Return:
+        - Success: {"email": "<user email>", "message": "Password updated"}
+        - Error: 403
+    """
+    email = request.form.get("email")
+    new_password = request.form.get("new_password")
+    token = request.form.get("reset_token")
+    try:
+        AUTH.update_password(token, new_password)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "message": "Password updated"})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -89,6 +89,18 @@ class Auth:
             user = self._db.find_user_by(email=email)
             token = _generate_uuid()
             self._db.update_user(user.id, reset_token=token)
-        except (NoResultFound):
+        except (ValueError, NoResultFound, InvalidRequestError):
             raise ValueError
         return token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update user password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            pwd = _hashed_password(password)
+            self._db.update_user(user.id,
+                                 hashed_password=pwd, reset_token=None)
+        except (ValueError, InvalidRequestError, NoResultFound):
+            raise ValueError
+        return None
