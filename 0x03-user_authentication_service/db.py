b31551ca6b13e2d2f82943d8c46cbcce872ceee3
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-01-29 14:10:13 +0100

    fix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -58,7 +58,7 @@ class DB:
         result = self.find_user_by(id=user_id)
         for k, v in kwargs.items():
             if hasattr(result, k):
-                result.k = v
+                result[k] = v
             else:
                 raise ValueError
         self._session.commit()
