9155ac3e56a644c966c06c8381f70bf55e517923
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-01-29 12:07:07 +0100

    added credential validation to auth

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """ Flask app api module
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", strict_slashes=False)
@@ -14,6 +16,22 @@ def get_handler():
         - Json payload
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def register_new_user():
+    """POST /users
+       Return:
+        - Success: {"email": "<registered email>", "message": "user created"}
+        - Failure: {"message": "email already registered"}
+    """
+    email = request.form.get("email")
+    passwd = request.form.get("password")
+    try:
+        user = AUTH.register_user(email, passwd)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": email, "message": "user created"})
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -13,17 +13,28 @@ def _hash_password(password: str) -> bytes:
 
 
 class Auth:
-    """Auth class to interact with the authentication database.
+    """Auth class implement authentical interfaces to DB class.
     """
 
     def __init__(self):
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """regsiter new user to db
+        """regsiter new user to DB
         """
         try:
             new_user = self._db.find_user_by(email=email)
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Verify if a User given User and password exist on the DB
+        """
+        try:
+            fs = self._db.find_user_by(email=email)
+            if fs:
+                return b.checkpw(password.encode("utf-8"), fs.hashed_password)
+        except (ValueError, NoResultFound):
+            pass
+        return False
